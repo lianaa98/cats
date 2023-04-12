fb66b9fdb0e1e48bdf99bf183c4279421e7ec884
lianaa98 <liana.kan98@gmail.com>
2023-04-11 20:02:09 -0700

    implemented mocha testing and revised callback

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,11 +2,14 @@ const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, callback) {
 
+  if (!breed || !callback) return;
+
   console.log('breedDetailsFromFile: Calling readFile...');
 
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
 
     console.log("In readFile's Callback: it has the data.");
+    if (error) callback();
     if (!error) callback(data);
   });
 };
@@ -15,4 +18,6 @@ const printCatBreed = breedData => {
   console.log('Return Value: ', breedData);
 }
 
-breedDetailsFromFile('Bombay', printCatBreed);
+module.exports = breedDetailsFromFile;
+
+// breedDetailsFromFile('Bombay', printCatBreed);
