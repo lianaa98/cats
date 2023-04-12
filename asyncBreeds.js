cd2df4a5341aa0d470e3be54651801654ef08db4
lianaa98 <liana.kan98@gmail.com>
2023-04-11 18:41:47 -0700

    initial commit for callback

---
+++ asyncBreeds.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+
+  console.log('breedDetailsFromFile: Calling readFile...');
+
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) callback(data);
+  });
+};
+
+const printCatBreed = breedData => {
+  console.log('Return Value: ', breedData);
+}
+
+breedDetailsFromFile('Bombay', printCatBreed);
